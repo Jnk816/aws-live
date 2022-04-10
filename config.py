f9e58c4c0d99c6d62931a0635524ddcf600ced7a
Jnk816 <68592937+Jnk816@users.noreply.github.com>
2022-04-10 17:27:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cpaymdnjm2bm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jordanngkentzu-employee"
+customregion = "us-east-1"
